585d987bbe508f2389b43d2ad4ce6f439d3cd1e9
DanielHayashi-tech <72723716+DanielHayashi-tech@users.noreply.github.com>
2021-02-10 11:59:40 -0600

    Create zylab1_20.py

---
+++ Homework 1/zylab1_20.py
@@ -0,0 +1,7 @@
+user_num = int(input('Enter Integer: '))
+print('You entered: ', user_num)
+print(user_num, 'squared is', user_num*user_num)
+print('And', user_num, 'cubed is', user_num*user_num*user_num, '!!')
+user_num2 = int(input('Enter another integer: '))
+print(user_num,'+',user_num2, 'is', user_num+user_num2)
+print(user_num,'*',user_num2, 'is', user_num*user_num2)
